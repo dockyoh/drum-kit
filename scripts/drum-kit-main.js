7b586f9e4e673e6fab8833a247fe539ecc7e3dd3
Reygin <sreygin@gmail.com>
2026-04-21 23:00:08 +0800

    Play the sound once even if pressing the key for too long

--- scripts/drum-kit-main.js
+++ scripts/drum-kit-main.js
@@ -1,4 +1,6 @@
 window.addEventListener("keydown", (event) => {
+  if (event.repeat) return;
+
   const keyElement = document.querySelector(`.key[data-key="${event.key}"]`);
   const audioElement = document.querySelector(`audio[data-key="${event.key}"]`);
 
