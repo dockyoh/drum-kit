61e1bb7a0551b8acb07d6f498d1d83ce710c83fa
Reygin <sreygin@gmail.com>
2026-04-21 22:46:35 +0800

    Play audio back from start even if pressing the key faster

--- scripts/drum-kit-main.js
+++ scripts/drum-kit-main.js
@@ -1,6 +1,11 @@
 window.addEventListener("keydown", (event) => {
   const keyElement = document.querySelector(`.key[data-key="${event.key}"]`);
   const audioElement = document.querySelector(`audio[data-key="${event.key}"]`);
-  console.log(audioElement);
-  audioElement.play();
+
+  playAudio(audioElement);
 });
+
+function playAudio(audioElement) {
+  audioElement.currentTime = 0;
+  audioElement.play();
+}
