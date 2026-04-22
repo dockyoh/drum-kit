d100e884b12364d5242e432f80aa593fa4a61529
Reygin <sreygin@gmail.com>
2026-04-22 22:14:03 +0800

    Added more instrument sound and key style when pressed

--- scripts/drum-kit-main.js
+++ scripts/drum-kit-main.js
@@ -5,9 +5,15 @@ window.addEventListener("keydown", (event) => {
   const audioElement = document.querySelector(`audio[data-key="${event.key}"]`);
 
   playAudio(audioElement);
+  renderStyle(keyElement);
 });
 
 function playAudio(audioElement) {
   audioElement.currentTime = 0;
   audioElement.play();
+}
+
+function renderStyle(keyElement) {
+  console.log(keyElement);
+  keyElement.classList.add("playing");
 }
