d8627689eaa393f634d1c347815b7797f1375d31
Reygin <sreygin@gmail.com>
2026-04-25 00:02:09 +0800

    Organize code v2

--- scripts/drum-kit-main.js
+++ scripts/drum-kit-main.js
@@ -1,17 +1,17 @@
-import { playAudio, renderStyle } from "./drum-kit.js";
+import { Play } from "./drum-kit.js";
 
 window.addEventListener("keydown", (event) => {
   Play(event);
 });
 
-function Play(event) {
-  if (event.repeat) return;
+// function Play(event) {
+//   if (event.repeat) return;
 
-  const keyElement = document.querySelector(`.key[data-key="${event.key}"]`);
-  const audioElement = document.querySelector(`audio[data-key="${event.key}"]`);
+//   const keyElement = document.querySelector(`.key[data-key="${event.key}"]`);
+//   const audioElement = document.querySelector(`audio[data-key="${event.key}"]`);
 
-  if (keyElement) {
-    playAudio(audioElement);
-    renderStyle(keyElement);
-  }
-}
+//   if (keyElement) {
+//     playAudio(audioElement);
+//     renderStyle(keyElement);
+//   }
+// }
--- scripts/drum-kit.js
+++ scripts/drum-kit.js
@@ -1,11 +1,23 @@
-export function playAudio(audioElement) {
+function playAudio(audioElement) {
   audioElement.currentTime = 0;
   audioElement.play();
 }
 
-export function renderStyle(keyElement) {
+function renderStyle(keyElement) {
   keyElement.classList.add("playing");
   keyElement.addEventListener("transitionend", (event) => {
     keyElement.classList.remove("playing");
   });
+}
+
+export function Play(event) {
+  if (event.repeat) return;
+
+  const keyElement = document.querySelector(`.key[data-key="${event.key}"]`);
+  const audioElement = document.querySelector(`audio[data-key="${event.key}"]`);
+
+  if (keyElement) {
+    playAudio(audioElement);
+    renderStyle(keyElement);
+  }
 }
